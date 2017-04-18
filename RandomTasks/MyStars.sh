3abc0620971430ad024b2f0e58f45a14adea7cdd
Roxana Yaneva <roxy.1569@gmail.com>
2017-04-18 23:52:50 +0300

    Initial commit

---
+++ RandomTasks/MyStars.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#print
+#
+#  ***
+#   **
+#    *
+
+N=$1
+
+for (( i=${N};i>=0;i--))
+do
+	for ((k=${N}-1;k>=${i};k--))
+	do
+		echo -n " "
+	done
+	for (( j=0;j<=${i};j++))
+	do
+		echo -n "*"
+	done
+	echo ""
+done
+
