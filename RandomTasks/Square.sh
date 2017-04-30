67aa4c40d9502f45e9054dca60b8887a5ddcf530
Roxana Yaneva <roxy.1569@gmail.com>
2017-05-01 01:58:16 +0300

    initial commit

--- RandomTasks/Square.sh
+++ RandomTasks/Square.sh
@@ -3,9 +3,9 @@
 numbers=("$@")
 n=$#
 
-for (( i=0;i<n;i++))
+for (( i=0;i<{n};i++))
 do
 	number=${numbers[${i}]}
-	echo $(($number*$number))
+	echo $((${number}*${number}))
 done
 
--- RandomTasks/TaskWithFilesAndDirs.sh
+++ RandomTasks/TaskWithFilesAndDirs.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-string=$1
-number=$2
+string=${1}
+number=${2}
 currentDir=$(ls)
 
-for item in $currentDir
+for item in "${currentDir}"
 do
-	if [ -d $item ] && [ $item == $string ]
+	if [ -d "${item}" ] && [ "${item}" == "${string}" ]
 	then
-		subdir=$item
-		cd $subdir
+		subdir="${item}"
+		cd "${subdir}"
 		for var in $(ls)
 		do
-			numberOfSymbols=$(wc -c $var | cut -d ' ' -f 1)
-			if [ $numberOfSymbols -gt $number ]
+			numberOfSymbols=$(wc -c "${var}" | cut -d ' ' -f 1)
+			if [ ${numberOfSymbols} -gt ${number} ]
 			then
-				echo $var
+				echo "${var}"
 			fi
 		done
 	else
-		echo "This is not a directory or is not named like $string: $item."
+		echo "This is not a directory or is not named like ${string}: ${item}."
 	fi
 done
 
