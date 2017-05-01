1d67af49e7d0ef437d53c15855d8ae9b88cabbad
Roxana Yaneva <roxy.1569@gmail.com>
2017-05-01 03:30:36 +0300

    initial commit

---
+++ 01_05_2017/guess.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+a=${1}
+b=${2}
+
+cnt=1
+
+if [ ${a} -lt ${b} ] ; then 
+	echo "Ok!"
+else
+	echo "Not ok!"
+fi
+
+number=$(( (RANDOM % ${b}) + ${a} ))
+
+echo -n "Guess? "
+read guess
+while [ ${guess} -ne ${number} ] 
+do
+	let cnt=${cnt}+1
+	if [ ${guess} -lt ${number} ] ; then
+		echo ...bigger!
+	else
+		echo ...smaller!
+	fi
+	echo -n "Guess again? "
+	read guess
+done
+
+echo "RIGHT! Guessed ${number} in ${cnt} tries!"
