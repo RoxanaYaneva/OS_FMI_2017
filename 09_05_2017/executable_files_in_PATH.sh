9dbf86b9118007360509413dec7f8d876bf8d9df
Roxana Yaneva <roxy.1569@gmail.com>
2017-05-09 17:15:53 +0300

    Update two scripts

--- 09_05_2017/executable_files_in_PATH.sh
+++ 09_05_2017/executable_files_in_PATH.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+DIRS=$(echo ${PATH} | tr ':' ' ')
+OLD_IFS="${IFS}"
+CNT=0
+
+for DIR in "${DIRS}" ; do
+	while IFS= read -d $'\0' -r FILE ; do
+		let CNT=${CNT}+1
+	done < <(find "${DIR}" -executable -type f -print0)
+done
+
+echo "Number of executable files: ${CNT}."
+
+IFS="${OLD_IFS}"
+
--- 09_05_2017/term_user_process.sh
+++ 09_05_2017/term_user_process.sh
@@ -5,4 +5,12 @@ if [ $# -ne 1 ] ; then
 	exit 1
 fi
 
+USER="${1}"
+
+ALL_PROCESSES=$(ps aux | grep -v "ssh\|bash" | tr -s ' ' | cut -d ' ' -f 1,2 | grep "${USER}" | cut -d ' ' -f 2)
+
+for PROCESS in "${ALL_PROCESSES}" ; do
+	echo "Killing process ${PROCESS}..."
+	pkill "${PROCESS}"
+done
 
