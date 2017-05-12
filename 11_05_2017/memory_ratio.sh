b4a15315908367ecfac7d84276a997458f24afd0
Roxana Yaneva <roxy.1569@gmail.com>
2017-05-12 12:47:26 +0300

    changed memory_ratio.sh

--- 11_05_2017/memory_ratio.sh
+++ 11_05_2017/memory_ratio.sh
@@ -6,10 +6,9 @@ USER_PROCESSES=$(ps aux | tr -s ' ' | sort -n -k 5 | grep "${USER}")
 
 for PROCESS in "${USER_PROCESSES}" ; do
 
-	PID=$(echo PROCESS | cut -d ' ' -f 2)
-	RSS=$(echo PROCESS | cut -d ' ' -f 6)
-	VSZ=$(echo PROCESS | cut -d ' ' -f 5)
+	PID=$(echo ${PROCESS} | cut -d ' ' -f 2)
+	RSS=$(echo ${PROCESS} | cut -d ' ' -f 6)
+	VSZ=$(echo ${PROCESS} | cut -d ' ' -f 5)
 
 	echo "${PID} : $(echo "scale=2; ${RSS}/${VSZ}" | bc)"
 done
-
